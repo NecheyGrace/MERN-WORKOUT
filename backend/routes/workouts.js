650e57e61b86a260f2124f8b946b9ad4afff0fb9
somate10000 <somate10000@gmail.com>
2023-04-20 12:41:14 +0100

    created model_schema, linked to mogodb.

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,7 +1,37 @@
 const express = require("express");
-
+const Workout = require("../moedels/workoutModel");
+// require the express router
 const router = express.Router();
 
-router.get("/", () => {});
+// get all wokouts handlers
+router.get("/", (req, res) => {
+  res.json({ mssg: "GET ALL WORKOUTS" });
+});
 
+// get a single workorequt
+router.get("/:id", (req, res) => {
+  res.json({ mssg: "GET a single Workout" });
+});
+
+// post a single workorequt
+router.post("/", async (req, res) => {
+  const { title, load, reps } = req.body;
+
+  try {
+    const workout = await Workout.create({ title, load, reps });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// delete a single workorequt
+router.delete("/:id", (req, res) => {
+  res.json({ mssg: "delete a new Workout" });
+
+  // post a single workorequt
+  router.patch("/:id", (req, res) => {
+    res.json({ mssg: "UPDATE a new Workout" });
+  });
+});
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,33 @@
+// covers the entry file for the bk app and register our expess app
+// npm init -y install depndencies
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 // express app
 const app = express();
+
+// middleware
+app.use(express.json());
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
 //routes
-app.use("/workouts", workoutRoutes);
-// listen for requests
+app.use("/api/workouts", workoutRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log("listening for requests on port 4000");
-});
+// connect to database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      //async func, so we have to listen for requests
+      console.log("listening for requests on port", process.env.PORT);
+    });
+  })
+
+  .catch((error) => {
+    console.log(error);
+  });
